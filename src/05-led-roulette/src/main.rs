fe95d7716b55bb17ce392b07659f2dfe823c348e
Robert Kalmar <rkalmar@cloudera.com>
2021-08-26 18:35:23 +0200

    Done LED roulette

--- src/05-led-roulette/src/main.rs
+++ src/05-led-roulette/src/main.rs
@@ -2,14 +2,25 @@
 #![no_main]
 #![no_std]
 
-use aux5::entry;
+use aux5::{entry, Delay, DelayMs, LedArray, OutputSwitch};
 
 #[entry]
 fn main() -> ! {
-    let _y;
-    let x = 42;
-    _y = x;
+    let (mut delay, mut leds): (Delay, LedArray) = aux5::init();
 
-    // infinite loop; just so we don't leave this stack frame
-    loop {}
+    let delay_ms = 50u32;
+
+    leds[0].on().unwrap();
+    leds[1].on().unwrap();
+    let mut index = 1;
+
+    loop {
+        delay.delay_ms(delay_ms);
+        let prev_index = (index as isize - 1 + leds.len() as isize) as usize % leds.len();
+        leds[prev_index].off().unwrap();
+        delay.delay_ms(delay_ms);
+        let next_index = (index + 1) % leds.len();
+        leds[next_index].on().unwrap();
+        index = next_index;
+    }
 }
